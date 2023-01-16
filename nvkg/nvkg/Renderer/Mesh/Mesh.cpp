57bb68c7591b1033c8eb9d4eea4dbc6c39c93e1b
ilumary <christoph.britsch@icloud.com>
2023-01-16 21:39:24 +0100

    refactored mesh class

--- nvkg/nvkg/Renderer/Mesh/Mesh.cpp
+++ nvkg/nvkg/Renderer/Mesh/Mesh.cpp
@@ -10,125 +10,48 @@ namespace nvkg {
         return left.color == right.color && left.position == right.position && left.uv == right.uv;
     }
 
-    Mesh::Mesh() {}
+    Mesh::Mesh()
+        : vertex_buffer_(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT), index_buffer_(VK_BUFFER_USAGE_INDEX_BUFFER_BIT) {}
 
-    Mesh::Mesh(const MeshData& meshData) {
+    Mesh::Mesh(const MeshData& meshData)
+        : vertex_buffer_(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT), index_buffer_(VK_BUFFER_USAGE_INDEX_BUFFER_BIT) {
         load_vertices(meshData);
     }
 
-    Mesh::~Mesh() {
-        Buffer::destroy_buffer(vertex_staging_buffer_);
-        Buffer::destroy_buffer(index_staging_buffer_);
-
-        Buffer::destroy_buffer(vertex_buffer_);
-        if (has_index_buffer_) { Buffer::destroy_buffer(index_buffer_); }
-    }
+    Mesh::~Mesh() {}
 
     void Mesh::load_vertices(const Mesh::MeshData& meshData) {
         vertex_count_ = meshData.vertexCount;
         index_count_ = meshData.indexCount;
 
-        Buffer::create_buffer(
-            meshData.vertexSize * meshData.vertexCount,
-            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-            // specifies that data is accessible on the CPU.
-            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | 
-            // Ensures that CPU and GPU memory are consistent across both devices.
-            VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-            OUT vertex_staging_buffer_.buffer,
-            OUT vertex_staging_buffer_.bufferMemory);
-
-        Buffer::create_buffer(
-            sizeof(uint32_t) * meshData.indexCount,
-            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-            // specifies that data is accessible on the CPU.
-            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | 
-            // Ensures that CPU and GPU memory are consistent across both devices.
-            VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-            OUT index_staging_buffer_.buffer,
-            OUT index_staging_buffer_.bufferMemory);
+        vertex_buffer_.init_staging_buffer(meshData.vertexSize * meshData.vertexCount);
+        index_buffer_.init_staging_buffer(sizeof(uint32_t) * meshData.indexCount);
 
         has_vertex_buffer_ = meshData.vertexCount > 0;
 
-        if (has_vertex_buffer_) create_vertex_buffer(meshData.vertices, (meshData.vertexSize * meshData.vertexCount));
+        if(has_vertex_buffer_) vertex_buffer_.create_buffer(meshData.vertices, (meshData.vertexSize * meshData.vertexCount));
 
         has_index_buffer_ = meshData.indexCount > 0;
 
-        if (has_index_buffer_) create_index_buffer(meshData.indices, (sizeof(uint32_t) * meshData.indexCount));
-    }
-
-    void Mesh::create_vertex_buffer(const void* vertices, size_t size) {
-        Buffer::copy_data(vertex_staging_buffer_, size, vertices);
-
-        Buffer::create_buffer(
-            size,
-            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
-            // specifies that data is accessible on the CPU.
-            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-            OUT vertex_buffer_.buffer,
-            OUT vertex_buffer_.bufferMemory
-        );
-
-        Buffer::copy_buffer(vertex_staging_buffer_.buffer, vertex_buffer_.buffer, size);
-    }
-
-    void Mesh::create_index_buffer(const uint32_t* indices, size_t size) {
-        Buffer::copy_data(index_staging_buffer_, size, indices);
-
-        Buffer::create_buffer(
-            size,
-            VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
-            // specifies that data is accessible on the CPU.
-            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-            index_buffer_.buffer,
-            index_buffer_.bufferMemory
-        );
-
-        Buffer::copy_buffer(index_staging_buffer_.buffer, index_buffer_.buffer, size);
+        if(has_index_buffer_) index_buffer_.create_buffer(meshData.indices, (sizeof(uint32_t) * meshData.indexCount));
     }
 
     void Mesh::bind(VkCommandBuffer commandBuffer) {
         if (vertex_count_ > 0) {
-            VkBuffer buffers[] = {vertex_buffer_.buffer};
+            VkBuffer buffers[] = {vertex_buffer_.buffer_.buffer};
             VkDeviceSize offsets[] = {0};
             vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
         }
 
-        if (has_index_buffer_) vkCmdBindIndexBuffer(commandBuffer, index_buffer_.buffer, 0, VK_INDEX_TYPE_UINT32);
+        if (has_index_buffer_) vkCmdBindIndexBuffer(commandBuffer, index_buffer_.buffer_.buffer, 0, VK_INDEX_TYPE_UINT32);
     }
 
     void Mesh::update_vertices(const Mesh::MeshData& meshData) {
         vertex_count_ = meshData.vertexCount;
         index_count_ = meshData.indexCount;
 
-        update_vertex_buffer(meshData.vertices, (meshData.vertexSize * meshData.vertexCount));
+        vertex_buffer_.update(meshData.vertices, (meshData.vertexSize * meshData.vertexCount));
 
-        update_index_buffer(meshData.indices, (sizeof(uint32_t) * meshData.indexCount));
-    }
-
-    void Mesh::update_vertex_buffer(const void* vertices, size_t size) {
-        if (size == 0) return;
-
-        if (!has_vertex_buffer_) {
-            create_vertex_buffer(vertices, size);
-            has_vertex_buffer_ = true;
-            return;
-        }
-
-        Buffer::copy_data(vertex_staging_buffer_, size, vertices);
-        Buffer::copy_buffer(vertex_staging_buffer_.buffer, vertex_buffer_.buffer, size);
-    }
-
-    void Mesh::update_index_buffer(uint32_t* indices, size_t size) {
-        if (size == 0) return;
-
-        if (!has_index_buffer_) {
-            create_index_buffer(indices, size);
-            has_index_buffer_ = true;
-            return;
-        }
-
-        Buffer::copy_data(index_staging_buffer_, size, indices);
-        Buffer::copy_buffer(index_staging_buffer_.buffer, index_buffer_.buffer, size);
+        index_buffer_.update(meshData.indices, (sizeof(uint32_t) * meshData.indexCount));
     }
 }
--- nvkg/nvkg/Renderer/Mesh/Mesh.hpp
+++ nvkg/nvkg/Renderer/Mesh/Mesh.hpp
@@ -29,6 +29,56 @@ namespace nvkg {
     bool operator==(const Vertex& left, const Vertex& right);
     bool operator==(const Vertex2D& left, const Vertex2D& right);
 
+    struct staged_buffer {
+        Buffer::Buffer staging_buffer_, buffer_;
+        VkBufferUsageFlagBits buffer_usage_;
+
+        staged_buffer(VkBufferUsageFlagBits buffer_usage) {
+            buffer_usage_ = buffer_usage;
+        }
+
+        ~staged_buffer() {
+            Buffer::destroy_buffer(staging_buffer_);
+            Buffer::destroy_buffer(buffer_);
+        }
+
+        void init_staging_buffer(VkDeviceSize size) {
+            Buffer::create_buffer(size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 
+                VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, 
+                staging_buffer_.buffer, staging_buffer_.bufferMemory);
+        }
+
+        void create_buffer(const void* data, std::size_t size) {
+            if(size == 0) {
+                logger::debug(logger::Level::Error) << "Tried creating buffer with zero data";
+                return;
+            }
+
+            Buffer::copy_data(staging_buffer_, size, data);
+
+            Buffer::create_buffer(
+                size,
+                buffer_usage_ | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                // specifies that data is accessible on the CPU.
+                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                OUT buffer_.buffer,
+                OUT buffer_.bufferMemory
+            );
+
+            Buffer::copy_buffer(staging_buffer_.buffer, buffer_.buffer, size);
+        }
+
+        void update(const void* data, std::size_t size) {
+            if(size == 0) {
+                logger::debug(logger::Level::Error) << "Tried updating buffer with zero data";
+                return;
+            } 
+
+            Buffer::copy_data(staging_buffer_, size, data);
+            Buffer::copy_buffer(staging_buffer_.buffer, buffer_.buffer, size);
+        }
+    };
+
     class Mesh {
         public:
 
@@ -59,15 +109,8 @@ namespace nvkg {
 
         private:
 
-            void update_vertex_buffer(const void* vertices, size_t size);
-            void update_index_buffer(uint32_t* indices, size_t size);
-
-            void create_vertex_buffer(const void* vertices, size_t size);
-            void create_index_buffer(const uint32_t* indices, size_t size);
-
-            Buffer::Buffer vertex_staging_buffer_, index_staging_buffer_;
-
-            Buffer::Buffer vertex_buffer_, index_buffer_;
+            staged_buffer vertex_buffer_;
+            staged_buffer index_buffer_;
 
             bool has_index_buffer_ = false, has_vertex_buffer_ = false;
 
